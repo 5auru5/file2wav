61c5e0f7027bebfffa200ad71422c94e8afe5cd7
Arya Habibi <5auru5@users.noreply.github.com>
2021-03-23 22:06:39 -0500

    Update wav.py

--- wav.py
+++ wav.py
@@ -2,14 +2,29 @@ import sys
 import wave
 import struct
 import cgi
+#import argparse
+#import textwrap
 def main():
-    with open(sys.argv[1], "rb") as inp_f:
+    with open(sys.argv[1], "rb") as inp_f: #opens given file
         data = inp_f.read()
         with wave.open(sys.argv[2], "wb") as out_f:
-            out_f.setnchannels(1)
-            out_f.setsampwidth(2) # number of bytes
-            out_f.setframerate(int(sys.argv[3]))
+            out_f.setnchannels(int(sys.argv[4])) # number of channels
+            out_f.setsampwidth(int(sys.argv[5])) # number of bytes in sample width
+            out_f.setframerate(int(sys.argv[3]))  # sample rate
             out_f.writeframesraw(data)
 
+def printUsage():
+        print('''\
+Usage: Python3 wav.py [inputfilename] [outputfilename] [bitrate] [channels] [samplewidth]
+        
+        ''')
+
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except IndexError:
+        print("FAILURE: Please ensure all arguments are supplied")
+        printUsage()
+    except FileNotFoundError:
+        print("FAILURE: Please check your input file path")
+        printUsage()
